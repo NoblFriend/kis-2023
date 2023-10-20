90ab7d1e7d68e0d8772c68f8151aec824acae343
Alexey Rebrikov <rebrikov.av@phystech.edu>
2023-10-20 23:32:22 +0300

    Remake interface

--- src/modules/interface.py
+++ src/modules/interface.py
@@ -1,29 +1,28 @@
 import tkinter as tk
-import os
 from tkinter import ttk
 from tkinter import simpledialog
-from modules.characters import Character
 from datetime import datetime
-from tkcalendar import Calendar
+from tkcalendar import Calendar, DateEntry
 from tkintermapview import TkinterMapView
 
+from modules.models import Character, Geotag  
+from modules.sqlite_manager import CharacterAppDatabase 
 
 class GandalfApp:
     def __init__(self, root, db):
         self.root = root
-        self.db = db
+        self.db : CharacterAppDatabase = db
         self.root.title("Gandalf's Tracking App")
         self.create_main_window()
+        self.marker_path = None
         
-    def create_main_window(
-        self):
-
+    def create_main_window(self):
         self.main_frame = ttk.Frame(self.root, padding="10")
         self.main_frame.grid(row=0, column=0, sticky=(tk.W, tk.E, tk.N, tk.S))
         
         self.tree = ttk.Treeview(
             self.main_frame, 
-            columns=("ID", "Name"), 
+            columns=("ID", "Name"),  
             show="headings"
         )
         self.tree.heading("ID", text="ID")
@@ -35,6 +34,7 @@ class GandalfApp:
             text="Add Character",
             command=self.add_character_popup
         ).grid(row=1, column=0)
+
         ttk.Button(
             self.main_frame,
             text="Add Geotag",
@@ -59,12 +59,13 @@ class GandalfApp:
             
         characters = self.db.get_characters()
         for char in characters:
-            self.tree.insert("", tk.END, values=(char.id, char.name))
+            self.tree.insert("", tk.END, values=(char.id[:6], char.name))
             
     def add_character_popup(self):
         name = simpledialog.askstring("Input", "Enter the character name:")
         if name:
             new_char = Character(name=name)
+            print(new_char)
             self.db.add_character(new_char)
             self.refresh_characters()
             
@@ -72,48 +73,57 @@ class GandalfApp:
         characters = self.db.get_characters()
         
         if not characters:
-            tk.messagebox.showerror(
-                "Error", 
-                "No characters available. Please add a character first."
-            )
+            tk.messagebox.showerror("Error", "No characters available. Please add a character first.")
             return
-        
+
         char_names = [char.name for char in characters]
-        char_ids = [char.id for char in characters]
         
         dialog = tk.Toplevel(self.root)
         dialog.title("Add Geotag")
         
         ttk.Label(dialog, text="Select Character:").grid(row=0, column=0)
         selected_char = tk.StringVar(value=char_names[0])
-        char_dropdown = ttk.Combobox(
-            dialog, 
-            textvariable=selected_char, 
-            values=char_names
-        )
+        char_dropdown = ttk.Combobox(dialog, textvariable=selected_char, values=char_names)
         char_dropdown.grid(row=0, column=1)
-
-
+        
         ttk.Label(dialog, text="Select Date:").grid(row=1, column=0)
         cal = Calendar(dialog, selectmode="day", year=2023, month=10, day=20)
         cal.grid(row=1, column=1)
         
-        selected_date_label = ttk.Label(dialog, text="Selected date will appear here")
-        selected_date_label.grid(row=1, column=2)
-        
-        def on_date_select(event):
-            selected_date_label["text"] = cal.get_date()
-            
-        cal.bind("<<CalendarSelected>>", on_date_select)
 
+        def validate_time(P):
+            if P == "":
+                return True
+            try:
+                value = int(P)
+            except ValueError:
+                return False
+            if 0 <= value <= 59:  
+                return True
+            return False
+
+        def validate_hour(P):
+            if P == "":
+                return True
+            try:
+                value = int(P)
+            except ValueError:
+                return False
+            if 0 <= value <= 23: 
+                return True
+            return False
+
+        vcmd_time = dialog.register(validate_time)
+        vcmd_hour = dialog.register(validate_hour)
+        
         ttk.Label(dialog, text="Select Time:").grid(row=2, column=0)
         time_frame = ttk.Frame(dialog)
-        hour_entry = ttk.Entry(time_frame, width=3)
-        minute_entry = ttk.Entry(time_frame, width=3)
-        second_entry = ttk.Entry(time_frame, width=3)
-        hour_entry.grid(row=1, column=1)
-        minute_entry.grid(row=1, column=2)
-        second_entry.grid(row=1, column=3)
+        hour_entry = ttk.Entry(time_frame, validate='key', validatecommand=(vcmd_hour, '%P'), width=3)
+        minute_entry = ttk.Entry(time_frame, validate='key', validatecommand=(vcmd_time, '%P'), width=3)
+        second_entry = ttk.Entry(time_frame, validate='key', validatecommand=(vcmd_time, '%P'), width=3)
+        hour_entry.grid(row=0, column=0)
+        minute_entry.grid(row=0, column=1)
+        second_entry.grid(row=0, column=2)
         time_frame.grid(row=2, column=1)
         
         ttk.Label(dialog, text="Latitude:").grid(row=3, column=0)
@@ -126,7 +136,9 @@ class GandalfApp:
         
         def add_geotag():
             selected_char_name = selected_char.get()
-            if selected_char_name not in char_names:
+            selected_char_obj = next((char for char in characters if char.name == selected_char_name), None)
+            
+            if selected_char_obj is None:
                 tk.messagebox.showerror("Error", "Selected character not found.")
                 return
 
@@ -137,8 +149,9 @@ class GandalfApp:
                 tk.messagebox.showerror("Error", "Invalid latitude or longitude.")
                 return
 
-            selected_date = cal.get_date()
+            selected_date = cal.get_date().split("/")
             try:
+                month, day, year = map(int, selected_date)
                 hour = int(hour_entry.get())
                 minute = int(minute_entry.get())
                 second = int(second_entry.get())
@@ -146,17 +159,18 @@ class GandalfApp:
                 tk.messagebox.showerror("Error", "Invalid time format.")
                 return
 
-            timestamp = f"{selected_date} {hour}:{minute}:{second}"
+            timestamp = datetime(year+2000, month, day, hour, minute, second).isoformat()
             
-            char_id = char_ids[char_names.index(selected_char_name)]
-            self.db.add_geotag(char_id, timestamp, latitude, longitude)
+            new_geotag = Geotag(character_id=selected_char_obj.id, timestamp=timestamp, latitude=latitude, longitude=longitude)
+            self.db.add_geotag(new_geotag)
             dialog.destroy()
+
+        
             
-        ttk.Button(dialog, text="Add", command=add_geotag).grid(
-            row=5, columnspan=4)
+        ttk.Button(dialog, text="Add", command=add_geotag).grid(row=5, columnspan=4)
+
 
     def show_map_popup(self):
-
         self.map_widget = TkinterMapView(
             width=800, 
             height=600, 
@@ -164,20 +178,91 @@ class GandalfApp:
             max_zoom=15, 
             database_path="database/offline_tiles.db"
         )
-        self.map_widget.grid(row=0, column=0, columnspan=3, sticky="nsew")
+        self.map_widget.grid(row=0, column=0, sticky="nsew")
 
         close_button = tk.Button(self.map_widget, text="Close", command=self.map_widget.destroy)
         close_button.grid(row=0, column=1)
 
-        
         characters = self.db.get_characters()
-        for char in characters:
-            geotags = self.db.get_geotags(char.id)
+        char_names = [char.name for char in characters]
+        marker_list = []
+
+        ttk.Label(self.map_widget, text="From:").grid(row=1, column=0)
+        start_date_entry = DateEntry(self.map_widget)
+        start_date_entry.grid(row=1, column=1)
+
+        start_hour_entry = ttk.Entry(self.map_widget, width=3)
+        start_hour_entry.grid(row=1, column=2)
+        start_hour_entry.insert('0', '00')
+        start_minute_entry = ttk.Entry(self.map_widget, width=3)
+        start_minute_entry.grid(row=1, column=3)
+        start_minute_entry.insert('0', '00')
+        start_second_entry = ttk.Entry(self.map_widget, width=3)
+        start_second_entry.grid(row=1, column=4)
+        start_second_entry.insert('0', '00')
+
+        ttk.Label(self.map_widget, text="To:").grid(row=2, column=0)
+        end_date_entry = DateEntry(self.map_widget)
+        end_date_entry.grid(row=2, column=1)
+        
+        end_hour_entry = ttk.Entry(self.map_widget, width=3)
+        end_hour_entry.grid(row=2, column=2)
+        end_hour_entry.insert('0', '00')
+        end_minute_entry = ttk.Entry(self.map_widget, width=3)
+        end_minute_entry.grid(row=2, column=3)
+        end_minute_entry.insert('0', '00')
+        end_second_entry = ttk.Entry(self.map_widget, width=3)
+        end_second_entry.grid(row=2, column=4)
+        end_second_entry.insert('0', '00')
+
+        def clear():
+            for marker in marker_list:
+                self.map_widget.delete(marker)
+            marker_list.clear()
+
+            if self.marker_path is not None:
+                self.map_widget.delete(self.marker_path)
+
+        def update_map():
+            clear()
+            selected_char_name = selected_char.get()
+            if selected_char_name not in char_names:
+                return
+            char_id = characters[char_names.index(selected_char_name)].id
+            geotags = self.db.get_geotags(char_id)
+
+            start_date = start_date_entry.get_date()
+            start_time = f"{start_hour_entry.get()}:{start_minute_entry.get()}:{start_second_entry.get()}"
+            start_datetime = datetime.combine(start_date, datetime.strptime(start_time, '%H:%M:%S').time())
             
-            for geotag in geotags:
+            end_date = end_date_entry.get_date()
+            end_time = f"{end_hour_entry.get()}:{end_minute_entry.get()}:{end_second_entry.get()}"
+            end_datetime = datetime.combine(end_date, datetime.strptime(end_time, '%H:%M:%S').time())
+            
+
+            filtered_geotags = [geotag for geotag in geotags if ( geotag['timestamp'] <= end_datetime) and (geotag['timestamp'] >= start_datetime)]
+            sorted_geotags = sorted(filtered_geotags, key=lambda x: x['timestamp'])
+            
+            for geotag in sorted_geotags:
                 marker = self.map_widget.set_position(geotag['latitude'], geotag['longitude'], marker=True)
-                marker.text = char.name + ' ' + geotag['timestamp']
+                marker.text = selected_char_name + ' ' + str(geotag['timestamp'])
+                marker_list.append(marker)
             
-            if len(geotags) > 1:
-                positions = [(geotag['latitude'], geotag['longitude']) for geotag in geotags]
-                self.map_widget.set_path(positions)
+            if len(sorted_geotags) > 1:
+                positions = [(geotag['latitude'], geotag['longitude']) for geotag in sorted_geotags]
+                self.marker_path = self.map_widget.set_path(positions)
+
+
+        selected_char = tk.StringVar(value=char_names[0] if char_names else "")
+        char_dropdown = ttk.Combobox(
+            self.map_widget, 
+            textvariable=selected_char, 
+            values=char_names
+        )
+        char_dropdown.grid(row=0, column=2)
+
+        update_button = tk.Button(self.map_widget, text="Update Map", command=update_map)
+        update_button.grid(row=0, column=3)
+
+        if char_names:
+            update_map()
